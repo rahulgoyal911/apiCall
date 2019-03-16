cd7899eab48c6387096cd3ad9850db25ce861b6d
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-03-16 09:52:30 +0530

    weather

--- create.py
+++ create.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 # conn = psycopg2.connect(database = "testdb2", user = "postgresql", password = "namespace1", host = "sample-database.czgprnseypbr.us-east-1.rds.amazonaws.com", port = "5432")
-conn = psycopg2.connect(database = "postgres", user = "postgres", password = "namespace1", hostaddr="35.198.246.100", port = "5432")
+conn = psycopg2.connect(database = "weather", user = "postgres", password = "namespace1", hostaddr="35.198.246.100", port = "5432")
 print ('Opened database successfully')
 cur = conn.cursor()
 
